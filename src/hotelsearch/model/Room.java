d70800706674be9fbf4545c8cf78c23acb0442c9
JonasHakon <jonashakon01@gmail.com>
2022-03-22 19:49:40 +0000

    Útfæra Room klasan

--- src/hotelsearch/model/Room.java
+++ src/hotelsearch/model/Room.java
@@ -1,4 +1,35 @@
 package hotelsearch.model;
 
 public class Room {
+
+    // Variables
+    private int roomID;
+    private int hotelID;
+    private int nrBeds;
+    private int pricePerNight;
+
+    // Getters
+    public int getRoomID() {
+        return roomID;
+    }
+
+    public int gethotelID() {
+        return hotelID;
+    }
+
+    public int getNrBeds() {
+        return nrBeds;
+    }
+
+    public int getPricePerNight() {
+        return pricePerNight;
+    }
+
+    // Constructor
+    public Room(int roomid, int hotelid, int nrbeds, int pricepernight) {
+        roomID = roomid;
+        hotelID = hotelid;
+        nrBeds = nrbeds;
+        pricePerNight = pricepernight;
+    }
 }
