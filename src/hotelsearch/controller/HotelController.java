ce9943193465a58da68c56bc73a1a05be18fdeca
Nikhil Kumar <nikhil7e@gmail.com>
2022-03-27 19:51:03 +0000

    Add HotelController class template

--- src/hotelsearch/controller/HotelController.java
+++ src/hotelsearch/controller/HotelController.java
@@ -1,4 +1,23 @@
 package hotelsearch.controller;
 
+import hotelsearch.model.Booking;
+import hotelsearch.model.Hotel;
+
+import java.util.Date;
+import java.util.List;
+
 public class HotelController {
+
+    public List<Hotel> findHotels(String nameOrLocation, Date checkInDate, Date checkOutDate, int nrGuests) {
+        return null;
+    }
+
+    public List<Booking> bookRoom(Hotel hotel, String guestName, Date checkInDate, Date checkOutDate, int nrGuests) {
+        return null;
+    }
+
+    public List<Hotel> orderByPriceAscending(List<Hotel> list) {
+        return null;
+    }
+
 }
