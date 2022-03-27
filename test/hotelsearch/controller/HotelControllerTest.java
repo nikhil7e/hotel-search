7ef5ece5dad77da5be297a0fd1eb576df262027b
Nikhil Kumar <nikhil7e@gmail.com>
2022-03-27 17:27:46 +0000

    Implement orderByPriceAscending test case and remove booking case

--- test/hotelsearch/controller/HotelControllerTest.java
+++ test/hotelsearch/controller/HotelControllerTest.java
@@ -1,48 +1,49 @@
 package hotelsearch.controller;
 
-import hotelsearch.model.Booking;
 import hotelsearch.model.Hotel;
-import org.junit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
-
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class HotelControllerTest {
 
     @Before
     public void setUp() {
-
     }
 
     @After
     public void tearDown() {
-
     }
 
-    @Test(expected = SomeException)
+    @Test(expected = SQLException.class)
     public void testFindHotelConnectionFail() {
         DatabaseService mockDatabaseService = new NoConnectionMock();
         HotelController hotelController = new HotelController(mockDatabaseService);
         hotelController.findHotels();
     }
 
-    @Test(expected = SomeException)
+    @Test(expected = SQLException.class)
     public void testModifyBookingConnectionFail() {
         DatabaseService mockDatabaseService = new NoConnectionMock();
         HotelController hotelController = new HotelController(mockDatabaseService);
         hotelController.findHotels();
     }
 
-    @Test(expected = SomeException)
+    @Test(expected = SQLException.class)
     public void testCancelBookingConnectionFail() {
         DatabaseService mockDatabaseService = new NoConnectionMock();
         HotelController hotelController = new HotelController(mockDatabaseService);
         hotelController.findHotels();
     }
 
-    @Test(expected = SomeException)
+    @Test(expected = SQLException.class)
     public void testBookRoomConnectionFail() {
         DatabaseService mockDatabaseService = new NoConnectionMock();
         HotelController hotelController = new HotelController(mockDatabaseService);
@@ -61,18 +62,30 @@ public class HotelControllerTest {
     public void testFindHotelsFail() {
         DatabaseService mockDatabaseService = new UnsuccessfulSearchMock();
         HotelController hotelController = new HotelController(mockDatabaseService);
-        List<Hotel> hotelList = hotelController.findHotels("", new Date(10), new Date(11), 2);
+        List<Hotel> hotelList = hotelController.findHotels("/", new Date(10), new Date(11), 2);
         assertNotNull(hotelList);
         assertEquals(0, hotelList.size());
     }
 
     @Test
-    public void testModifyBookingSuccess() {
-        DatabaseService mockDatabaseService = new UnsuccessfulSearchMock();
-        HotelController hotelController = new HotelController(mockDatabaseService);
-        Booking originalBooking = hotelController.bookRoom("Hilton Hotel", new Date(9), new Date(11), 2);
-        Booking modifiedBooking = hotelController.modifyBooking(originalBooking.getBookingID(), new Date(10), originalBooking.getCheckOutDate());
-        assertEquals(modifiedBooking.getCheckInDate(), new Date(10));
+    public void testOrderByPriceAscending() {
+        DatabaseService databaseService = new DatabaseService();
+        HotelController hotelController = new HotelController(databaseService);
+        List<Hotel> hotelList = new ArrayList<>();
+        hotelList.add(new Hotel(123, "Test", 5, null, "",
+                1, 1, 1));
+        hotelList.add(new Hotel(12, "Test2", 5, null, "",
+                2, 1, 1));
+        hotelList.add(new Hotel(13, "Test3", 5, null, "",
+                3, 1, 1));
+
+        List<Hotel> orderedHotelList = hotelController.orderByPriceAscending(hotelList);
+        assertNotNull(hotelList);
+        for (int i = 1; i < orderedHotelList.size(); i++) {
+            Hotel current = orderedHotelList.get(i);
+            Hotel previous = orderedHotelList.get(i - 1);
+            assertTrue(current.getRoomPrice() >= previous.getRoomPrice());
+        }
     }
 
 }
