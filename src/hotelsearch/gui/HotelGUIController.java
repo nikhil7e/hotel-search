c9100d8d32a84af018719df2559966239604f4bb
Sakkinn <isakmar00@gmail.com>
2022-04-06 16:18:20 +0000

    Implement hotel search and hotel search result GUI.

---
+++ src/hotelsearch/gui/HotelGUIController.java
@@ -0,0 +1,92 @@
+package hotelsearch.gui;
+
+import hotelsearch.model.Hotel;
+import hotelsearch.model.HotelDB;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import org.w3c.dom.html.HTMLObjectElement;
+
+import java.sql.*;
+
+import javax.swing.table.TableColumn;
+import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class HotelGUIController implements Initializable {
+
+    @FXML
+    private DatePicker fxDateIn;
+    @FXML
+    private DatePicker fxDateOut;
+    @FXML
+    private TextField fxNrOfGuests;
+    @FXML
+    private TextField fxLocation;
+    @FXML
+    private TableView<Hotel> fxHotelTable;
+    @FXML
+    private Button fxSearchButton;
+    @FXML
+    private javafx.scene.control.TableColumn<Hotel, Image> fxImageColumn;
+    @FXML
+    private javafx.scene.control.TableColumn<Hotel, Integer> fxIDColumn;
+    @FXML
+    private javafx.scene.control.TableColumn<Hotel, String> fxLocationColumn;
+    @FXML
+    private javafx.scene.control.TableColumn<Hotel, Integer> fxStarsColumn;
+    @FXML
+    private javafx.scene.control.TableColumn<Hotel, String> fxDescriptionColumn;
+    @FXML
+    private javafx.scene.control.TableColumn<Hotel, Double> fxRoomPriceColumn;
+    @FXML
+    private javafx.scene.control.TableColumn<Hotel, String> fxDFDowntownColumn;
+    @FXML
+    private javafx.scene.control.TableColumn<Hotel, String> fxDFSupermarketColumn;
+    @FXML
+    private javafx.scene.control.TableColumn<Hotel, String> fxRestaurantColumn;
+    @FXML
+    private javafx.scene.control.TableColumn<Hotel, Boolean> fxBreakfastColumn;
+    @FXML
+    private javafx.scene.control.TableColumn<Hotel, Boolean> fxBarColumn;
+    @FXML
+    private javafx.scene.control.TableColumn<Hotel, Boolean> fxWifiColumn;
+
+    HotelDB test = new HotelDB();
+
+    ObservableList<Hotel> hotelObservableList = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        fxImageColumn.setCellValueFactory(new PropertyValueFactory<>("image"));
+        fxIDColumn.setCellValueFactory(new PropertyValueFactory<>("hotelID"));
+        fxLocationColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        fxStarsColumn.setCellValueFactory(new PropertyValueFactory<>("numberOfStars"));
+        fxDescriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+        fxRoomPriceColumn.setCellValueFactory(new PropertyValueFactory<>("startingRoomPrice"));
+        fxDFDowntownColumn.setCellValueFactory(new PropertyValueFactory<>("distanceFromDowntown"));
+        fxDFSupermarketColumn.setCellValueFactory(new PropertyValueFactory<>("distanceFromSupermarket"));
+    }
+
+    public void searchHandler(ActionEvent actionEvent) throws SQLException {
+        LocalDate dateIn = fxDateIn.getValue();
+        LocalDate dateOut = fxDateOut.getValue();
+        String location = fxLocation.getText();
+        int nrOfGuests = Integer.parseInt(fxNrOfGuests.getText());
+        List<Hotel> list = test.search(location, dateIn, dateOut, nrOfGuests);
+        hotelObservableList.addAll(list);
+        fxHotelTable.setItems(hotelObservableList);
+    }
+}
---
+++ src/hotelsearch/gui/HotelGUIMain.java
@@ -0,0 +1,25 @@
+package hotelsearch.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HotelGUIMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("HotelGUI.fxml"));
+        Parent root = loader.load();
+        HotelGUIController sc = loader.getController();
+        primaryStage.setTitle("HotelSearch");
+        Scene scene = new Scene(root, 1000, 550);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
